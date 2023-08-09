c86c7789155826ff40841fced641f888ff5428f1
季春二九 <64247113+jichun29@users.noreply.github.com>
2023-08-09 14:54:29 +0800

    Create gonghanglijian.js

---
+++ ShaNiu/gonghanglijian.js
@@ -0,0 +1,19 @@
+/**
+* @author 二九
+* @origin 季春二九
+* @create_at 2022-09-09 09:45:26
+* @description 🐒工行月月刷微信立减金定时推送
+* @version v1.0.0
+* @title 工行月月刷微信立减金
+* @platform qq wx tg pgm web cron
+* @rule 工行
+* @priority 100
+* @public false
+* @version v1.1.0
+* @icon https://img.jichun29.cn/img/202307171052077.ico
+*/
+
+
+var ele = "https://img.jichun29.cn/img/ea311f7fc76dc0c04f98cba507eadca.png";
+//图文消息
+s.reply("\n工行储蓄卡月月刷微信立减金" + image(ele))
